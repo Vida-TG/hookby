a2cf76d809a4ef5e0f2487b6b3a89572621149bc
ViDA_TG <victorademiju.t@gmail.com>
2023-01-22 19:37:38 +0100

    Random

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path")
 
 const router = express.Router;
 
 router.get('/users', (req, res) => {
-
+    res.send('../Views/index.ejs');
 })
 
 module.exports = router
