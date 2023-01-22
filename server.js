0bf59d02e7a67953a5a0d6afb3282d39263f3307
ViDA_TG <victorademiju.t@gmail.com>
2023-01-22 19:12:59 +0100

    Routes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
+const userRouter = require('./Routes/userRoute')
+
 const app = express()
 
+app.use('/', userRouter)
+
 app.listen(7000, () => {
-    console.log("Started")
+    console.log("Started");
 })
