bd16dca095515d04728f86ae87073046905278e3
ViDA_TG <victorademiju.t@gmail.com>
2023-01-23 06:59:40 +0100

    DB

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const userRouter = require('./Routes/userRoute')
 
 const app = express()
 app.set('view engine', 'ejs')
 
-app.use('/', userRouter)
+mongoose.connect(process.env.DBURi,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+)
+    .then((result) => app.listen(7000))
+    .catch((err) => console.log("DB connect err"));
 
-app.listen(7000, () => {
-    console.log("Started");
-})
+app.use('/', userRouter)
