b763c8f58c0a3f1796286b25e38ffd3f7e0f30d1
Vida-TG <victorademiju.t@gmail.com>
2023-03-03 15:10:22 +0100

    Server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs')
 
 mongoose.connect(process.env.DBURi)
     .then((result) => app.listen(8080))
-    .catch((err) => console.log("Database connection error"));
+    .catch((err) => console.log("There was a database connection error"));
 
 app.use(express.urlencoded({extended:false}))
 app.use('/', userRouter)
